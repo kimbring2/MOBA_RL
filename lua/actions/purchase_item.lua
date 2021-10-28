807f6da14e434bac159bf2e66966a4ca25a63517
Dohyeong Kim <kimbring2@gmail.com>
2021-10-28 20:20:00 +0900

    Update purchase_item.lua

--- lua/actions/purchase_item.lua
+++ lua/actions/purchase_item.lua
@@ -8,42 +8,53 @@ PurchaseItem.NumArgs = 3
 local tableItemsToBuy = { 
 				"item_tango",
 				"item_tango",
-				"item_clarity",
-				"item_clarity",
-				"item_branches",
-				"item_branches",
-				"item_magic_stick",
-				"item_circlet",
-				"item_boots",
-				"item_energy_booster",
-				"item_staff_of_wizardry",
-				"item_ring_of_regen",
-				"item_recipe_force_staff",
-				"item_point_booster",
-				"item_staff_of_wizardry",
-				"item_ogre_axe",
-				"item_blade_of_alacrity",
-				"item_mystic_staff",
-				"item_ultimate_orb",
-				"item_void_stone",
-				"item_staff_of_wizardry",
-				"item_wind_lace",
-				"item_void_stone",
-				"item_recipe_cyclone",
-				"item_cyclone",
 			};
 
 
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
 ----------------------------------------------------------------------------------------------------
 
-function PurchaseItem:Call(hUnit, item, item_name)
-	--local npcBot = GetBot();
-    hUnit.sideShopMode = true;
+function PurchaseItem:Call( hUnit, item, item_name )
+    --hUnit.sideShopMode = true;
+    --print("type(item): ", type(item))
+    --print("item: ", item)
+    --print("dump(item): ", dump(item))
+    --print("tostring(item_name): ", tostring(item_name))
 
 	if ( #tableItemsToBuy == 0 )
 	then
 		hUnit:SetNextItemPurchaseValue( 0 );
 		return;
+	end
+
+	local sNextItem = tableItemsToBuy[1];
+	--local sNextItem = "item_tango"
+
+	hUnit:SetNextItemPurchaseValue( GetItemCost( sNextItem ) );
+
+	if ( hUnit:GetGold() >= GetItemCost( sNextItem ) )
+	then
+		hUnit:ActionImmediate_PurchaseItem( sNextItem );
+		table.remove( tableItemsToBuy, 1 );
+	end
+
+end
+
+----------------------------------------------------------------------------------------------------
+return PurchaseItem
+
 	end
 
 	local sNextItem = tableItemsToBuy[1];
