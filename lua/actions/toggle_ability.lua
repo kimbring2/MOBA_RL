0f7f8339914da0373ef3392a62ee3ca22f42ef1a
Dohyeong Kim <kimbring2@gmail.com>
2021-12-02 17:09:05 +0900

    Add files via upload

--- lua/actions/toggle_ability.lua
+++ lua/actions/toggle_ability.lua
@@ -9,12 +9,21 @@ ToggleAbility.NumArgs = 2
 
 -------------------------------------------------
 function ToggleAbility:Call( hUnit, intAbilitySlot )
-    local hAbility = hUnit:GetAbilityInSlot(intAbilitySlot[1])
+    --local hAbility = hUnit:GetAbilityInSlot(intAbilitySlot[1])
+
+    if intAbilitySlot[1] >= 0 then
+        hAbility = hUnit:GetAbilityInSlot(intAbilitySlot[1])
+    else
+        itemSlot = -intAbilitySlot[1] - 1
+        --print("dump(itemSlot): ", dump(itemSlot))
+        hAbility = hUnit:GetItemInSlot(itemSlot)
+    end
+
     if not hAbility then
         print('[ERROR]: ', hUnit:GetUnitName(), " failed to find ability in slot ", intAbilitySlot[1])
         do return end
     end
-    hAbility:ToggleAutoCast()
+    hAbility:ToggleAbility()
 end
 -------------------------------------------------
 
